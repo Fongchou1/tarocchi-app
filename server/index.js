85e8fb227f80f912eaca5c793a6c7c3ceb13f5ba
Fong Feng-En Chou <m82530@gmail.com>
2024-02-07 23:47:50 +0100

    to update stack

--- server/index.js
+++ server/index.js
@@ -10,29 +10,6 @@ var base = new Airtable({apiKey: process.env.AIRTABLE_ACCESS}).base(process.env.
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 6000;
 
-//for heroku error
-// const http = require('http');
-
-// process
-//   .on('SIGTERM', shutdown('SIGTERM'))
-//   .on('SIGINT', shutdown('SIGINT'))
-//   .on('uncaughtException', shutdown('uncaughtException'));
-
-// setInterval(console.log.bind(console, 'tick'), 1000);
-// http.createServer((req, res) => res.end('hi'))
-//   .listen(process.env.PORT || 6000, () => console.log('Listening'));
-
-// function shutdown(signal) {
-//   return (err) => {
-//     console.log(`${ signal }...`);
-//     if (err) console.error(err.stack || err);
-//     setTimeout(() => {
-//       console.log('...waited 5s, exiting.');
-//       process.exit(err ? 1 : 0);
-//     }, 5000).unref();
-//   };
-// }
-
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
   console.error(`Node cluster master ${process.pid} is running`);
